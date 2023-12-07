43e55d32372e0ecd6919ff91320015bf4c69c8f2
nietbaymuratbaev0999@gmail.com <nietbaymuratbaev0999@gmail.com>
2023-12-07 15:44:57 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter your name: ");
+string name = Console.ReadLine()!;
+string greeting = $"(Hello {name})";
+Console.WriteLine(greeting);
+Console.WriteLine("Enter your age: ");
+string ageAsString = Console.ReadLine()!;
+System.Console.WriteLine("Converting...");
+int age = Convert.ToInt32(ageAsString);
+System.Console.WriteLine($"Succesfully converted! {age}");
+int keschasAge = 3;
+int ageDiference = age -keschasAge;
+System.Console.WriteLine($"The difference between your and Kescha's age is {ageDiference}"); 
